9dd749e5f3d0445dc48a96579dbb5c88f5dcab30
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-20 06:34:05 +0300

    fix api interactions

--- bot/api_client.rb
+++ bot/api_client.rb
@@ -1,89 +1,69 @@
 require_relative '../config/config_wrapper'
 
 module API
-  API_BASE_URL = 'localhost:6789'.freeze
+  URL = 'localhost:6789'.freeze
 
   module_function
 
   def current_song
-    api_get('/current.json')
+    execute('current_song')
   end
 
   def playlists
-    api_get('/playlists.json')
+    execute('playlists')[:names]
   end
 
   def playlist_songs(name)
-    api_get('/playlist.json', params: { name: name })
+    execute('playlist', name: name)[:songs]
   end
 
   def next_track
-    api_put('/next.json', {})
+    execute('next')
   end
 
   def previous_track
-    api_put('/previous.json', {})
+    execute('previous')
   end
 
   def play
-    api_put('/play.json', {})
+    execute('play')
   end
 
   def pause
-    api_put('/pause.json', {})
+    execute('pause')
   end
 
   def volume
-    api_get('/volume.json')[:volume]
+    execute('status')[:volume]
   end
 
   def volume_up
-    api_put('/volume/up.json', {})[:volume]
+    execute('increase_volume', value: 25)[:volume]
   end
 
   def volume_down
-    api_put('/volume/down.json', {})[:volume]
+    execute('decrease_volume', value: 25)[:volume]
   end
 
   def update
-    api_post('/update.json', {})
+    execute('update')
   end
 
   def reload
-    api_post('/reload.json', {})
+    raise 'TODO'
   end
 
   def repeat
-    api_put('/repeat.json', {})
+    execute('repeat')
   end
 
   def random
-    api_put('/random.json', {})
+    execute('random')
   end
 
-  # api_get '/path'
-  def api_get(*args)
-    api_request(:get, args)
-  end
-
-  def api_put(*args)
-    api_request(:put, args)
-  end
-
-  def api_post(*args)
-    api_request(:post, args)
-  end
-
-  # api_request :get, ['/path']
-  def api_request(req_type, args)
-    args[0] = api_url(args[0])
-    args.unshift(req_type)
-    data = RestClient.send(*args)
-    JSON.parse(data, symbolize_names: true)
-  end
-
-  def api_url(path)
-    path = '/' + path unless path.start_with?('/')
-    API_BASE_URL + path
+  def execute(action, params = {})
+    data = params.merge(action: action)
+    response = RestClient.post(URL, data.to_json, content_type: :json)
+    JSON.parse(response, symbolize_names: true)
   end
 end
